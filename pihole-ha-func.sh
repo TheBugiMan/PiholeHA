66362de306d066cd51bd1c5e834178ea0aa27b7a
TheBugiMan <b6nome@hotmail.com>
2022-12-26 23:00:52 +1000

    building actual failover code

--- pihole-ha-func.sh
+++ pihole-ha-func.sh
@@ -73,3 +73,67 @@ function sendnotification_ifttt {
     curl -s --retry 5 https://maker.ifttt.com/trigger/pihole_ha/with/key/$ifttt_token?state=$notificationmessage
 }
 
+function flag_dhcpon {
+    echo ""
+    echo "Generating lock file for DHCP state"
+    touch ${dir}/dhcp.on
+    echo "...Done!"
+}
+
+function flag_dhcpoff {
+    echo ""
+    echo "Clearing lock file..."
+    rm -f ${dir}/dhcp.on
+    echo "...Done!"
+}
+
+# copy DNSmasq configuration from backup location to live configuration
+function dhcp_copyconf {
+    #cp ${dir}/dnsmasq/02-pihole-dhcp.conf /etc/dnsmasq.d/ # don't believe this is required?
+    #cp ${dir}/dnsmasq/03-pihole-dhcp-DNS.conf # no need to activate redundant DNS server config as only one instance currently active
+    #cp ${dir}/dnsmasq/04-pihole-static-dhcp.conf /etc/dnsmasq.d/ # managed with GravitySync
+    #cp ${dir}/dnsmasq/05-pihole-custom-cname.conf /etc/dnsmasq.d/ # managed through GravitySync
+
+    #copy existing leases
+    cp ${dir}/pihole/dhcp.leases /etc/pihole/
+}
+
+# import DNSmasq configuration from partner PiHole instance
+function dhcp_backupconf {
+    #rsync -ai ${syncuser}@${target}:/etc/dnsmasq.d/* ${dir}/dnsmasq/ #anything dnsmasq seems to be covered by GravitySync now
+    #rsync -ai ${syncuser}@${target}:/etc/pihole/* ${dir}/pihole  #FTL DB too big to sync this way, plus already managed through GravitySync
+    
+    # sync active DHCP leases, wrapped to detect any file changes
+    # this may be removed in future depending on outcomes of testing
+    RSYNC_COMMAND=$(rsync -ai ${syncuser}@${target}:/etc/pihole/dhcp.leases ${dir}/pihole)
+
+    if [ $? -eq 0 ]; then
+        # Success do some more work!
+
+        if [ -n "${RSYNC_COMMAND}" ]; then
+            # Stuff to run, because rsync has changes
+
+            # @@ set flag to refresh DNSmasq with SIGHUP or restart FTL?
+        else
+            # No changes were made by rsync
+        fi
+    else
+        # Something went wrong!
+        exit 1
+    fi
+}
+
+# Enable DHCP on local PiHole instance
+function piholedhcp_enable {
+    echo ""
+    echo "Enable DHCP on local PiHole instance..."
+    $pihole_app -a enabledhcp $piholedhcpparam
+    echo "...Done!"   
+}
+# Disable DHCP on local PiHole instance
+function piholedhcp_disable {
+    echo ""
+    echo "Disabling DHCP on local PiHole instance..."
+    $pihole_app -a disabledhcp
+    echo "...Done!"   
+}
